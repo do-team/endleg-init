011b2590a08cffeb7ed4c1fef27a85f27844c09c
Dave de Sade <davesade.42@gmail.com>
2017-03-22 16:30:38 +0100

    Testing...

--- index.js
+++ index.js
@@ -2,31 +2,31 @@ var AWS = require("aws-sdk");
 
 exports.handler = (event, context, callback) => {
 
-    console.log(event.userName);
+   // Cognito invokes this function, thus we got all user data we need.
+    var user = event.userName;
+    console.log(event);
 
     AWS.config.update({
         region: "eu-central-1",
         endpoint: "dynamodb.eu-central-1.amazonaws.com"
     });
 
-/*
-    var user = event.user;
+
     var params = {
         TableName: "endleg-score",
         Item: {
             "user": user,
         }
     };
+
     var dynamodb = new AWS.DynamoDB();
     var docClient = new AWS.DynamoDB.DocumentClient();
     docClient.put(params, function(err, data) {
         if (err) {
             console.log(err);
-            console.error("Unable to add new items from user ", user, ". Error JSON:", JSON.stringify(err, null, 2));
+            console.error("Unable to add new user ", user, "! Error JSON:", JSON.stringify(err, null, 2));
         } else {
-            console.log("Request by user", user, " was successfully added.");
+            console.log("New user ", user, " was successfully created.");
         }
     });
-*/
-
 };
